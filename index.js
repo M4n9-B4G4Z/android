04a2d830e83442e1e5b7825e4e6e287932cd3eb9
M4n9-B4G4Z <74099362+M4n9-B4G4Z@users.noreply.github.com>
2021-01-28 18:26:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -259,11 +259,23 @@ client.on('group-participants-update', async (anu) => {
 			console.log(anu)
 			if (anu.action == 'add') {
 				num = anu.participants[0]
+				try {
+					ppimg = await client.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.us`)
+				} catch {
+					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+				}
 				teks = `Hallo Kak @${num.split('@')[0]}\nSelamat datang di group *${mdata.subject}* yang betah di sini yaa✨.\nJANGAN LUPA TOP UP🔥\n*────────────────*\n❌NO SPAM BOT❌\nKetik untuk pilih kategori :\n> *List* (List Top up All Game)\n> *Pay* (list Pembayaran)\n> *Id* (Format via ID)\n> Login (Format via Login)`
+				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
 				num = anu.participants[0]
+				try {
+					ppimg = await client.getProfilePicture(`${num.split('@')[0]}@c.us`)
+				} catch {
+					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+				}
 				teks = `Akhirnya beban group berkurang 𝟭.\nBye bye🥳 @${num.split('@')[0]}\nJasamu akan di kubur dalam²`
+				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			}
 		} catch (e) {
